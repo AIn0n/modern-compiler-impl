b90fef84e632dced255d6cd8f8464bd2064e9a0f
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2025-11-20 00:03:05 +0100

    first exercise done

---
+++ tiger/src/chap1/exercises.h
@@ -0,0 +1,8 @@
+#ifndef _exercises_h_
+#define _exercises_h_
+
+#include "tokens.h"
+
+int maxargs(A_stm);
+
+#endif
---
+++ tiger/src/utils.h
@@ -0,0 +1,9 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+typedef char *string;
+string String(char *);
+
+void* checked_malloc(int);
+
+#endif
