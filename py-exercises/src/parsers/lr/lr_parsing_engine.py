db28779d81f0c6989f99352cd8ba23b02e656854
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2026-02-28 00:12:34 +0100

    minor scinka

--- py-exercises/src/parsers/lr/lr_parsing_engine.py
+++ py-exercises/src/parsers/lr/lr_parsing_engine.py
@@ -1,1 +1,14 @@
-class LREngine: ...
+from .lr0 import LR0Parser
+
+
+class LREngine:
+    def __init__(self, p: LR0Parser) -> None:
+        self.indexed_rules = p.indexed_rules
+        self.table = p.parsing_table
+
+    def parse(self, tokens: list[str]):
+        # stack = []
+        # TODO:
+        # * learn which state is the first one
+        # * finish this
+        ...
