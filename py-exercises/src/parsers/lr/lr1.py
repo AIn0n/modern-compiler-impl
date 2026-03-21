47daeb4f86e3ef68cd83513ffb6900a010a8dbe5
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2026-03-21 19:22:18 +0100

    LR(1) in progress

--- py-exercises/src/parsers/lr/lr1.py
+++ py-exercises/src/parsers/lr/lr1.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
-from parsers.base_parser import BaseParser, ParserPrintStyler, RuleType
+from parsers.base_parser import ParserPrintStyler, RuleType
+from parsers.lr.lr0 import LR0Parser
 
 @dataclass(slots=True, frozen=True)
 class LR1Item:
@@ -12,10 +13,41 @@ class LR1Item:
     during the printing.
     """
     rule: RuleType
+    # Assume 0 dot_pos is dot on the beginning of rhs
+    # R -> . A B C
+    # dot_pos = 0
+    # R -> A . B C
+    # dot_pos = 1
+    # and so on...
     dot_pos: int
     lookahead: str
 
+    def peek_after_dot(self) -> str | None:
+        if self.dot_pos >= len(self.rule.rhs):
+            return None
+        return self.rule.rhs[self.dot_pos]
 
-class LR1Parser(BaseParser):
-    def __init__(self, styling: ParserPrintStyler):
-        super().__init__(styling=styling)
+    def advance_dot(self) -> LR1Item:
+        return LR1Item(rule=self.rule, dot_pos=self.dot_pos + 1, lookahead=self.lookahead)
+
+
+LR1State = frozenset[LR1Item]
+
+
+class LR1Parser(LR0Parser):
+    def __init__(self, styling: ParserPrintStyler | None = None, end_symbol: str = "$"):
+        super().__init__(styling=styling, end_symbol=end_symbol)
+
+    def goto(self, i: LR1State, x: str) -> LR1State:
+        j = frozenset([el.advance_dot() for el in i if el.peek_after_dot() == x])
+        return self.closure(j)
+    
+    def closure(self, i: LR1State) -> LR1State:
+        while True:
+            new_i: set[LR1Item] = set(i)
+            for item in i:
+                x: str | None = item.peek_after_dot()
+                if x is None or x in self.terminals:
+                    continue
+                for rule in self.rules:
+                    ...
