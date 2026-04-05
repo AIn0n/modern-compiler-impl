8618654a5f16f7b505b48a09749209a121450947
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2026-04-05 20:11:32 +0200

    Removed unused file

--- py-exercises/src/parsers/commons.py
+++
@@ -1,8 +0,0 @@
-def swap_with_next(c: tuple, i: int) -> tuple:
-    """
-    swap tuple i element with the next one, return new tuple
-    """
-    j = i + 1
-    *begin, i_val = c[:j]
-    j_val, *end = c[j:]
-    return (*begin, j_val, i_val, *end)
